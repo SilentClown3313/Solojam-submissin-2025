a8ab1938c0ed855de9874bd9529296f2f32de84d
SilentClown3313 <jobaerkhan29122010@gmail.com>
2026-01-04 11:07:30 +0600

    Commit No.2

--- main.py
+++ main.py
@@ -4,30 +4,41 @@ import random
 from sys import exit
 
 #importing images
-#start_img = pygame.image.load("C:\Solojam-submissin-2025\IMG\Start_button.png")
 
 #Buttons
 class Button():
-    def __init__(self,x,y,image):
-        self.image = image
+    def __init__(self,x,y,scale,image):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image,(int(width*scale),(int(height*scale))))
         self.rect = self.image.get_rect()
         self.rect.topleft = x,y
+        self.get_clicked = False
 
     def draw(self):
+        action = False
+        #mouse pos
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] ==1 and self.get_clicked == False:
+                self.get_clicked = True
+                if self.get_clicked == True:
+                    action = True
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.get_clicked = False
+
         screen.blit(self.image,(self.rect.x, self.rect.y))
+        return action
 
 #Set-UP
 pygame.init()
 Clock = pygame.time.Clock()
 
 #Dsplay window
-scrn_width = 1200
-scrn_height = 600
+scrn_width = 600
+scrn_height = 400
 screen = pygame.display.set_mode((scrn_width, scrn_height))
-pygame.display.set_caption("Pong practice")
-
-#Game state variables:
-game_paused = False
+pygame.display.set_caption("Jailscape")
 
 #Game Colours
 BG_Colour = (90,90,90)
@@ -35,24 +46,15 @@ White = (255,255,255)
 
 #Text:
 game_font = pygame.font.SysFont("arialblack.ttf",40)
-text_surf_1 = game_font.render("Press SPACE to PAUSE", True, White)
+text_surf_1 = game_font.render("TEXT", True, White)
 
-#RECTS
-
+#Rects
 
 # Config
-
 
 while True:
 
     screen.fill(BG_Colour)
-
-    # chek game state:
-    if game_paused == False:
-        pass
-    else:
-        screen.fill(White)
-        pygame.display.set_caption("Pause Menu")
 
     #Exitting the game
     for event in pygame.event.get():
